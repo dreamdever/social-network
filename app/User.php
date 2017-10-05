6db063c9c4e6967c961a0a78c30f14526561a355
dreamdever <brieska.martin@gmail.com>
2017-10-05 02:13:55 +0200

    User Auth

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected $attributes = [
+        'description' => "",
+        'remember_token' => ""
+    ];
 }
